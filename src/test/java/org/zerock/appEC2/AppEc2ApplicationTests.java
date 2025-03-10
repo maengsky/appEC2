2ae8d4d400a33a3ef1d50804e3d57b8e3acf1898
DESKTOP-8Q87KGM\human-21 <gksmf2401@naver.com>
2025-03-10 11:49:36 +0900

    테스트 배포1

---
+++ src/test/java/org/zerock/appEC2/AppEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.appEC2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
